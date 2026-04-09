00810ec5c7e25e8800091e0e63c4e144177cd8fd
Tee <teelexuan@gmail.com>
2026-04-09 11:07:42 +0800

    Initial Commit

--- UECS2083_AM_05/src/strategy/EDFAlgorithm.java
+++ UECS2083_AM_05/src/strategy/EDFAlgorithm.java
@@ -3,7 +3,7 @@ package strategy;
 import java.util.*;
 import model.Delivery;
 
-// initialize task -> task priority -> scheduling -> execution -> all tasks? -> result aaa
+// initialize task -> task priority -> scheduling -> execution -> all tasks? -> result
 public class EDFAlgorithm extends AbstractDeliveryStrategy<Delivery> {
 
 	public EDFAlgorithm(List<Delivery> delivery) { 
